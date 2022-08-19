145733cefedfc9cd7d3df4b8f36a27202ca48105
shayan86a <111562719+shayan86a@users.noreply.github.com>
2022-08-19 01:02:28 -0700

    Add files via upload

---
+++ bigget & smallest.py
@@ -0,0 +1,12 @@
+num1 = input("please insert the frest number: ")
+num2 = input("please insert the scend number: ")
+num3 = input("please insert the third number: ")
+num4 = input("please insert the fourth number: ")
+num5 = input("please insert the fifth number: ")
+num6 = input("please insert the sixth number: ")
+num7 = input("please insert the seventh number: ")
+num8 = input("please insert the eighth number: ")
+num9 = input("please insert the ninth number: ")
+num10 = input("please insert the tenth number: ")
+number = [int(num1), int(num2), int(num3), int(num4), int(num5), int(num6), int(num7), int(num8), int(num9), int(num10)]
+print(min(number))
